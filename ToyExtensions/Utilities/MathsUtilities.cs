5b94d70522f59576bf8111d7c6cb7f04db8aefa5
sukeshchand <sukeshchand@hotmail.com>
2019-06-22 17:13:04 +0530

    initial commit - a maths utility function

---
+++ ToyExtensions/Utilities/MathsUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toy.Extensions.Utilities
+{
+    public static class MathsUtilities
+    {
+        public static List<long> GetSumOfPowerOfTwo(long n)
+        {
+
+            var reminders = new List<int>();
+            var powerOfTwo = new List<long>();
+            while (n > 0)
+            {
+                reminders.Add((int)n % 2);
+                n = n / 2;
+            }
+            for (var i = 0; i < reminders.Count; i++)
+            {
+                if (reminders[i] == 1)
+                {
+                    powerOfTwo.Add((long)Math.Pow(2, i));
+                }
+            }
+            return powerOfTwo;
+        }
+    }
+}
